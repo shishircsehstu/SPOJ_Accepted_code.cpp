2a65855b6ddcda28f19bc2a3f44e35997404077a
Md Saddam Hossain <shishir.cse.hstu@gmail.com>
2017-08-14 13:02:10 -0700

    Create Game Of Ones.cpp

---
+++ Game Of Ones.cpp
@@ -0,0 +1,76 @@
+//BISMILLAHIR RAHMANIR RAHIM
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<map>
+#include<string>
+#include<set>
+#include<cmath>
+#include<cctype>
+#include<stack>
+#include<cstdlib>
+#include<utility>
+#include<vector>
+#include<deque>
+#include<queue>
+#include<algorithm>
+
+#define sc scanf
+#define si(t) scanf("%d",&t)
+#define sl(t) scanf("%I64d",&t)
+#define sii(a,b) scanf("%d%d",&a,&b)
+
+#define P(a) printf("%d\n",a)
+#define PLN(a) printf("%I64d ",a)
+#define pf printf
+
+#define gcd(a,b) __gcd(a,b)
+#define ff first
+#define ss second
+#define pr1(a) cout<<a<<endl
+#define pr2(a,b) cout<<a<<" "<<b<<endl
+
+#define pii pair<int,int>
+#define pi acos(-1.0)
+#define PI 3.1415926535897932385
+#define Sin(a) sin((pi*a)/180)
+#define siz 1000001
+#define mem(ar) memset(ar,0,sizeof ar)
+#define one(x) __builtin_popcount(x)
+typedef long long ll;
+using namespace std;
+//int dx[]= {-1,-1,0,0,1,1};
+//int dy[]= {-1,0,-1,1,0,1};
+//int dx[]= {0,0,1,-1};/*4 side move*/
+//int dy[]= {-1,1,0,0};/*4 side move*/
+//int dx[]= {1,1,0,-1,-1,-1,0,1};/*8 side move*/
+//int dy[]= {0,1,1,1,0,-1,-1,-1};/*8 side move*/
+//int dx[]={1,1,2,2,-1,-1,-2,-2};/*night move*/
+//int dy[]={2,-2,1,-1,2,-2,1,-1};/*night move*/
+map<ll,bool>visi;
+long long ar[10001];
+long long  ar1[1001];
+void sive()
+{
+    ar[1]=ar1[1]=1;
+    long long n,s,s2,i;
+    for(i=2; i<=50; i++)
+    {
+        ar1[i]=ar1[i-1]*2+ar[i-1];
+        s=pow(2,i-1);
+        ar[i]=s;
+    }
+}
+int main()
+{
+    sive();
+    //freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+    long long i,j,n,s,s1,sum,t;
+    scanf("%lld",&t);
+    while(t--)
+    {
+        scanf("%lld",&n);
+        printf("%lld %lld\n",ar[n],ar1[n]);
+    }
+}
